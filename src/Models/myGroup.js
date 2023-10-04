2fdcb6fe9747c364585b1d4ddcb1811d9b220f3d
Zeniittt <tinnqd1702@gmail.com>
2023-10-05 00:23:02 +0700

    Week05

--- src/Models/myGroup.js
+++
@@ -1,8 +0,0 @@
-const myGroup = [
-    { id: 20110016, name: 'Nguyen Quoc Duc Tin' },
-    { id: 20110395, name: 'Huynh Ky Son' },
-    { id: 20110373, name: 'Ly Hong Khanh' },
-
-];
-
-module.exports = myGroup;
--- src/Routes/index.js
+++
@@ -1,7 +0,0 @@
-const siteRouter = require('./site');
-const userRouter = require('./user');
-function route(app) {
-    app.use('/users', userRouter);
-    app.use('/', siteRouter);
-}
-module.exports = route;
